d24293588fa66a57173119568af5efce33b307f2
ciwi <378361812@qq.com>
2018-02-08 10:55:45 +0800

    init

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './css/style.css'
+class Footer extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (<div className="foot"><span>Code by ciwi</span></div>);
+  }
+}
+
+export default Footer;
